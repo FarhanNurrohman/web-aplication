bba0286d2b2414810bc29898ce2d87c56c5e0319
FarhanNurrohman <farhan.nurrahman95@gmail.com>
2020-09-13 09:16:42 +0700

    menggunakan fungsi tambah() pada file tambah.php

--- tambah.php
+++ tambah.php
@@ -1,6 +1,15 @@
 <?php
 require "funsctions.php";
 
+// mengecek apakah button sumbit telah di pencet
+if(isset($_POST['submit'])){
+    $nama = $_POST['nama'];
+    $nis = $_POST['nis'];
+    $gambar = $_POST['gambar'];
+    $no_tlp = $_POST['no_tlp'];
+    tambah($nis, $nama, $no_tlp, $gambar);
+
+}
 
 
 
